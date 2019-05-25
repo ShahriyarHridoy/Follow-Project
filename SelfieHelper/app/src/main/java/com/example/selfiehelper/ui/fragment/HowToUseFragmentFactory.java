61d64caf2f2c56980e812156ab9f221050648ac0
ShahriyarHridoy <35798678+ShahriyarHridoy@users.noreply.github.com>
2019-05-26 00:24:22 +0600

    test project

---
+++ SelfieHelper/app/src/main/java/com/example/selfiehelper/ui/fragment/HowToUseFragmentFactory.java
@@ -0,0 +1,23 @@
+package com.example.selfiehelper.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.selfiehelper.R;
+
+public class HowToUseFragmentFactory extends Fragment {
+
+    public static HowToUseFragmentFactory newInstance(int position) {
+        Bundle args = new Bundle();
+        HowToUseFragmentFactory fragment = new HowToUseFragmentFactory();
+        args.putInt("htu_fragment", position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+}
